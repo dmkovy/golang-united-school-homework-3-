7a4d23c662c56d99a6a77e701443282b5dd4ba05
Dm <dmkhost@gmail.com>
2022-05-06 18:06:50 +0200

    change task01-arrays

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,8 +1,9 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	for i, value := range input {
-		result += value / float32(i+1)
+	for _, value := range input {
+		result += value
 	}
+	result /= 15
 	return
 }
